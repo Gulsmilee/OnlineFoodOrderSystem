cb6468f61472f57ff8da12d8c8e2cb1f9d1c9cad
Gülbahar <gulsmilee.401@gmail.com>
2025-12-29 19:10:19 +0300

    Test:Restaurant menusu test edildi kontroller saglandi.

--- OnlineFoodOrderSystem/src/com/oyes/Main.java
+++ OnlineFoodOrderSystem/src/com/oyes/Main.java
@@ -3,15 +3,12 @@ package com.oyes;
 public class Main {
 
     public static void main(String[] args) {
-
-    	
-    	System.out.println("--- SİSTEM TESTİ BAŞLIYOR ---\n");
+        System.out.println("--- SİSTEM TESTİ BAŞLIYOR ---\n");
 
         // 1. ADIM: User Testi
         System.out.println("1. User Testi:");
         User u1 = new User("001", "Ahmet Hoca", "ahmet@okul.edu", "1234");
         u1.login(); 
-        System.out.println(u1.toString());
         
         System.out.println("\n--------------------------\n");
 
@@ -19,28 +16,37 @@ public class Main {
         System.out.println("2. Customer Testi:");
         Customer c1 = new Customer("002", "Gülbahar", "gul@mail.com", "pass987", "Kadikoy, Istanbul", "555-1234");
         c1.login();
-        System.out.println("Müşteri: " + c1.getName() + " - Adres: " + c1.getAddress());
 
         System.out.println("\n--------------------------\n");
 
         // 3. ADIM: MenuItem (Yemek) Testi
         System.out.println("3. MenuItem Testi:");
-        
-        // Yeni bir yemek oluşturalım
-        MenuItem m1 = new MenuItem("Lahmacun", "Bol acili, citir", 120.0);
+        MenuItem m1 = new MenuItem("Adana Kebap", "Acili ve lezzetli", 220.0);
         MenuItem m2 = new MenuItem("Ayran", "Yayik ayran", 30.0);
+        MenuItem m3 = new MenuItem("Kunefe", "Peynirli sicak tatli", 150.0);
+        System.out.println("Yemek oluşturuldu: " + m1.getName());
+
+        System.out.println("\n--------------------------\n");
+
+        // 4. ADIM: Restaurant Testi (YENİ!)
+        System.out.println("4. Restaurant Testi:");
         
-        // toString() metodunu test edelim
-        System.out.println("Eklenen 1: " + m1.toString());
-        System.out.println("Eklenen 2: " + m2.toString());
+        // Restoranı oluşturuyoruz
+        Restaurant myRestaurant = new Restaurant("Lezzet Dunyasi", 4.8, "Besiktas, Istanbul");
+        System.out.println("Restoran Acildi: " + myRestaurant.getName());
         
-        // Interface metodlarını (getName, getPrice) test edelim
-        System.out.println("Sorgu: " + m1.getName() + " fiyati ne kadar? -> " + m1.getPrice() + " TL");
+        // Yemekleri menüye ekliyoruz
+        System.out.println(">> Yemekler menüye ekleniyor...");
+        myRestaurant.addMenuItem(m1); // Kebap
+        myRestaurant.addMenuItem(m2); // Ayran
+        myRestaurant.addMenuItem(m3); // Künefe
+        
+        // Menüyü ekrana basalım
+        System.out.println("\n--- GÜNCEL MENÜ ---");
+        for (MenuItem item : myRestaurant.getMenu()) {
+            System.out.println("- " + item.toString());
+        }
 
         System.out.println("\n--- TEST BAŞARIYLA TAMAMLANDI ---");
     }
-}	
-
-
-
-
+}
